6f505b7639f58211cc68c98a77ae73fbbacc96e1
MarioS271 <marioerik.scholze@gmail.com>
2026-07-25 03:24:53 +0200

    fix: im an idiot

--- src/kernel/src/arch/x86_64/tables/tss.rs
+++ src/kernel/src/arch/x86_64/tables/tss.rs
@@ -12,7 +12,6 @@ use x86_64::VirtAddr;
 
 pub static TASK_STATE_SEGMENT: Once<TaskStateSegment> = Once::new();
 
-static mut DOUBLE_FAULT_STACK: AlignedStack<8192> = AlignedStack{ array: [0u8; 8192] };
 static mut IST1_STACK: AlignedStack<8192> = AlignedStack{ array: [0u8; 8192] };
 static mut IST2_STACK: AlignedStack<8192> = AlignedStack{ array: [0u8; 8192] };
 static mut IST3_STACK: AlignedStack<8192> = AlignedStack{ array: [0u8; 8192] };
@@ -21,7 +20,6 @@ static mut IST4_STACK: AlignedStack<8192> = AlignedStack{ array: [0u8; 8192] };
 pub fn init() {
     // Safe because we are still single threaded at this point
     unsafe {
-        let df_stack_addr = VirtAddr::from_ptr(DOUBLE_FAULT_STACK.array.as_ptr().add(DOUBLE_FAULT_STACK.array.len()));
         let ist1_stack_addr = VirtAddr::from_ptr(IST1_STACK.array.as_ptr().add(IST1_STACK.array.len()));
         let ist2_stack_addr = VirtAddr::from_ptr(IST2_STACK.array.as_ptr().add(IST2_STACK.array.len()));
         let ist3_stack_addr = VirtAddr::from_ptr(IST3_STACK.array.as_ptr().add(IST3_STACK.array.len()));
@@ -30,11 +28,10 @@ pub fn init() {
         TASK_STATE_SEGMENT.call_once(|| {
             let mut tss = TaskStateSegment::new();
 
-            tss.interrupt_stack_table[0] = df_stack_addr;
-            tss.interrupt_stack_table[1] = ist1_stack_addr;
-            tss.interrupt_stack_table[2] = ist2_stack_addr;
-            tss.interrupt_stack_table[3] = ist3_stack_addr;
-            tss.interrupt_stack_table[4] = ist4_stack_addr;
+            tss.interrupt_stack_table[0] = ist1_stack_addr;
+            tss.interrupt_stack_table[1] = ist2_stack_addr;
+            tss.interrupt_stack_table[2] = ist3_stack_addr;
+            tss.interrupt_stack_table[3] = ist4_stack_addr;
 
             tss
         });
