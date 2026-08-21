f652ebdda6f2d980d794421b44e6d46f0be86414
MarioS271 <marioerik.scholze@gmail.com>
2026-08-21 16:14:56 +0200

    fix: remove KStateSubCategory trait

--- src/kernel/src/state/subcat.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-//! Marker trait for `KState` subcategory structs.
-//!
-//! Authors: MarioS271
-
-/// Marker trait implemented by every subsystem struct stored in [`KState`].
-pub trait KStateSubCategory {
-
-}
